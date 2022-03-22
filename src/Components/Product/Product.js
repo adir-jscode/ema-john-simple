efd2373b9de9c0edcf02412913d422f77ffe966b
adir-jscode <adir.jscode@gmail.com>
2022-03-23 00:13:50 +0600

    icon remaining

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -4,6 +4,7 @@ import './Product.css';
 const Product = (props) => {
     console.log(props.product);
     const { img, name, price, seller, ratings } = props.product;
+    const {product, handleCart } = props;
     return (
         <div className="product">
             <div className="product-image">
@@ -15,7 +16,9 @@ const Product = (props) => {
                 <p><small>Manufacturer:{seller}</small></p>
                 <p><small>Rating : { ratings}</small></p>
             </div>
-            
+            <button className='cart-btn' onClick={() => handleCart(product)}>
+                <p >Add to cart</p>
+            </button>
         </div>
     );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -4,24 +4,31 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart,setCart]=useState([]);
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
             .then(response => response.json())
-        .then(data => setProducts(data))
-    },[])
+        .then(data => setProducts(data.slice(0,20)))
+    }, [])
+    const handleCart = (products) => {
+        const newCart = [...cart, products];
+         setCart(newCart)
+    }
     return (
         <div className="shop-container">
-            <div>
-                <h1>Number of products {products.length}</h1>
+            <div className="product-container">
+                
                 {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleCart={handleCart}
                     ></Product>)
                 }
             </div>
-            <div>
+            <div className='order-container'>
                 <h1>Order Summary</h1>
+                <p>Product Selected: {cart.length }</p>
             </div>
         </div>
     );
